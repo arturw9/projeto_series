18825b84d9c02ea9a32c9ac28b7fc63d47796d84
FidelisArtur <arturfidelis@pactosolucoes.com.br>
2022-05-02 00:22:50 -0300

    MVC-Model

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('cabecalho')
+    Adicinar Série
+@endsection
+
+@section('conteudo')
+
+
+        <form method="post">
+            <div class="form-group">
+                <label for="nome" class="">Nome</label>
+                <input type="text" class="form-control" name="nome">
+            </div>
+
+            <button class="btn btn-primary">Adicionar</button>
+        </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
 <?php
 
 Route:: get('/series', 'App\Http\Controllers\SeriesController@index');
+Route:: get('/series/criar', 'App\Http\Controllers\SeriesController@create');
